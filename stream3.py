a1d3971f922044c1e71c27fd05defb99c78875ce
Ronnie Shida Marinho <113137267+ronniemarinho@users.noreply.github.com>
2026-05-20 13:08:13 -0300

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -3,22 +3,36 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 
-if not firebase_admin._apps:
+st.write("Python funcionando")
 
-    firebase_config = json.loads(st.secrets["firebase"])
+try:
 
-    cred = credentials.Certificate(firebase_config)
+    if not firebase_admin._apps:
 
-    firebase_admin.initialize_app(
-        cred,
-        {
-            "databaseURL":
-            "https://esp32-fe8e3-default-rtdb.firebaseio.com/"
-        }
-    )
+        firebase_config = json.loads(
+            st.secrets["firebase"]
+        )
 
-ref = db.reference("/")
+        cred = credentials.Certificate(
+            firebase_config
+        )
 
-dados = ref.get()
+        firebase_admin.initialize_app(
+            cred,
+            {
+                "databaseURL":
+                "https://esp32-fe8e3-default-rtdb.firebaseio.com/"
+            }
+        )
 
-st.write(dados)
+    ref = db.reference("/")
+
+    dados = ref.get()
+
+    st.success("Firebase OK")
+
+    st.write(dados)
+
+except Exception as e:
+
+    st.error(e)
